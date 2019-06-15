099c9ad1625ff5692248e4e53051e0529839053a
Kristoffer Panahon <krp312@me.com>
2019-06-15 16:11:34 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Square.destroy_all
+Game.destroy_all
+
+(1..10).each do |i|
+  (1..10).each do |j|
+    Square.new(position_x: i, position_y: j).save
+  end
+end
+
+g = Game.new
+g.save
+
+Game.last.update(status: 'waiting_for_player1', active_player: 1)
+
+Square.first.update(player1_ship?: true)
+Square.second.update(player1_ship?: true)
+Square.third.update(player2_ship?: true)
+Square.fourth.update(player2_ship?: true)
