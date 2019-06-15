c57d6586ab980762a638fa3941cacae4113f7c2c
Kristoffer Panahon <krp312@me.com>
2019-06-15 10:19:02 -0400

    Add Game table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_200715) do
+ActiveRecord::Schema.define(version: 2019_06_14_224605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "active_player"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "squares", force: :cascade do |t|
     t.integer "position_y"
