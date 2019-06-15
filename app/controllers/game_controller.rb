b49bc757b6141188aea1f5213aeefc4d5cff5522
Kristoffer Panahon <krp312@me.com>
2019-06-15 12:58:34 -0400

    Assign players

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,18 @@
 class GameController < ApplicationController
   def index
     @squares = Square.all.order(:id)
+    @game = Game.last
+    assign_players
     render :index
+  end
+
+  def assign_players
+    if @game.status == 'waiting_for_player1'
+      cookies[:player] = 'one'
+      @game.update(status: 'waiting_for_player2')
+    elsif @game.status == 'waiting_for_player2'
+      cookies[:player] = 'two'
+      @game.update(status: 'in_progress')
+    end
   end
 end
